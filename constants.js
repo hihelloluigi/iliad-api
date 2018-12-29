07ed309717dd2826a3c930214023a209d66b2dba
Matteo Crippa <matteo@boostco.de>
2018-12-29 17:37:19 +0100

    created constants file

---
+++ constants.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+  CURRENT_APP_VERSION: '26',
+  ILIAD_BASE_URL: 'https://www.iliad.it/account/',
+  ILIAD_OPTION_URL: {
+    login: 'attivazione-della-sim',
+    information: 'i-miei-dati-personali',
+    credit: 'consumi-e-credito',
+    voicemail: 'segreteria-telefonica',
+    options: 'le-mie-opzioni',
+    services: 'i-miei-servizi',
+    recharge: 'rechargement',
+    activation: 'attivazione-della-sim',
+    document: 'le-condizioni-della-mia-offerta',
+    recover: 'forget',
+  },
+  ILIAD_STORES: '',
+};
